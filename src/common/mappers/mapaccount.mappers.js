438ed99f1280976ff07f539761c0e87a23a8e0d0
Estela <estelajimero@users.noreply.github.com>
2021-08-27 12:00:27 +0200

    Common mappers

---
+++ src/common/mappers/mapaccount.mappers.js
@@ -0,0 +1,13 @@
+export const mapAccountFromApiToVM = account => {
+    return {
+        ...account,
+        alias: account.name,
+    };
+};
+
+export const mapAccountFromVMToApi = account => {
+    return {
+        ...account,
+        name: account.alias,
+    };
+};
